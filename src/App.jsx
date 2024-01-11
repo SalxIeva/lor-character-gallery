b3963b7d70eb2f792e162006b84b2c08db569626
SalxIeva <ieva.saldukaite@gmail.com>
2024-01-11 20:59:35 +0000

    ...

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import CharacterGallery from "./components/CharacterGallery";
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <h1 className="site-title">Lord of the Rings</h1>
+      <CharacterGallery />
+    </>
+  );
+}
+
+export default App
